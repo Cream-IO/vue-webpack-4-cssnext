4567f7cc98758ed813187394ff14e8ac07f53765
David Venin <david@hostnfly.com>
2018-05-07 01:13:42 +0200

    add vue, webpack 4, postcss with cssnext config

---
+++ src/components/App/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <h1 class="title">{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data () {
+      return {
+        title: 'Welcome to my starter !'
+      }
+    }
+  }
+</script>
+
+<style>
+  html {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  .title {
+    text-align: center;
+    @media (--tablet) {}
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import App from './components/App/App.vue'
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
